88b642fe0c3f440782700c0da980063314918777
Hiverton <hiverton.alves@gmail.com>
2021-08-07 16:48:06 -0300

    ajustes

--- src/main/java/br/com/coffeeandit/api/HomeController.java
+++ src/main/java/br/com/coffeeandit/api/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
     @GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
     public ResponseEntity<String> home() {
 
-        return ResponseEntity.ok("Hiverton!");
+        return ResponseEntity.ok("Hiverton Alves!");
 
     }
 
